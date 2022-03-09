19f978cb4d953bd463423018ff583f53976f6a72
adamclear <3589@holbertonschool.com>
2022-03-09 20:26:55 +0000

    updated file 100

--- 0x00-ssh/100-puppet_ssh_config.pp
+++ 0x00-ssh/100-puppet_ssh_config.pp
@@ -1,4 +1,4 @@
-ssh_authorized_key { ubuntu@54.91.142.100
+ssh_authorized_key { ubuntu@54.91.142.100:
     user => 'ubuntu',
     type => 'ssh-rsa',
     key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDNdtrNGtTXe5Tp1EJQop8mOSAuRGLjJ6DW4PqX4wId/Kawz35ESampIqHSOTJmbQ8UlxdJuk0gAXKk3Ncle4safGYqM/VeDK3LN5iAJxf4kcaxNtS3eVxWBE5iF3FbIjOqwxw5Lf5sRa5yXxA8HfWidhbIG5TqKL922hPgsCGABIrXRlfZYeC0FEuPWdr6smOElSVvIXthRWp9cr685KdCI+COxlj1RdVsvIo+zunmLACF9PYdjB2s96Fn0ocD3c5SGLvDOFCyvDojSAOyE70ebIElnskKsDTGwfT4P6jh9OBzTyQEIS2jOaE5RQq4IB4DsMhvbjDSQrP0MdCLgwkN',
